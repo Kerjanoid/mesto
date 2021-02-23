0a2680a5e7ad871eb330e273e846c1ed573553d0
Vyacheslav Bardakov <Kerjanoid@yandex.ru>
2021-02-23 23:03:38 +0300

    Практическая работа №4. ver.2

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,15 @@
-let editProfile = document.querySelector('.edit-button');
-let Popup = document.querySelector('.popup');
+let editProfile = document.querySelector('.profile__edit-button');
+let popup = document.querySelector('.popup');
 let closeButton = document.querySelector('.popup__close-button');
 let formElement = document.querySelector('.popup__content');
 let profileTitel = document.querySelector('.profile__title');
 let profileSubtitle = document.querySelector('.profile__subtitle');
-let inputFieldTitel = document.querySelector('.popup__input-field_titel');
-let inputFieldSubtitel = document.querySelector('.popup__input-field_subtitel');
-let like = document.querySelectorAll('.element__like');
+let inputFieldTitel = document.querySelector('.popup__input-field_type_titel');
+let inputFieldSubtitel = document.querySelector('.popup__input-field_type_subtitel');
+let likes = document.querySelectorAll('.element__like');
 
 function togglePopup() {
-  Popup.classList.toggle('popup_opened');
+  popup.classList.toggle('popup_opened');
   inputFieldTitel.value = profileTitel.textContent;
   inputFieldSubtitel.value = profileSubtitle.textContent;
 };
@@ -26,9 +26,9 @@ function changeProfile(event) {
 
 formElement.addEventListener('submit', changeProfile);
 
-for (let i= 0; i < like.length; i++) {
-  like[i].addEventListener('click', function() {
-    like[i].classList.toggle('element__like_active');
+for (let i= 0; i < likes.length; i++) {
+  likes[i].addEventListener('click', function() {
+    likes[i].classList.toggle('element__like_active');
   });
 }
 
