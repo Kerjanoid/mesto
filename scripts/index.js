081c36311c1d25abdbb90aae551fcc523eafaf28
Vyacheslav Bardakov <Kerjanoid@yandex.ru>
2021-03-07 17:15:42 +0300

    feat: add new popup

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,82 @@
-let editProfile = document.querySelector('.profile__edit-button');
-let popup = document.querySelector('.popup');
-let closeButton = document.querySelector('.popup__close-button');
-let formElement = document.querySelector('.popup__content');
-let profileTitel = document.querySelector('.profile__title');
-let profileSubtitle = document.querySelector('.profile__subtitle');
-let inputFieldTitel = document.querySelector('.popup__input-field_type_titel');
-let inputFieldSubtitel = document.querySelector('.popup__input-field_type_subtitel');
-let likes = document.querySelectorAll('.element__like');
+const editProfile = document.querySelector('.profile__edit-button');
+const popup = document.querySelector('.popup');
+const popupProfile = document.querySelector('.popup_edit-profile');
+const popupNewPic = document.querySelector('.popup_new-picture');
+const popupViewPic = document.querySelector('.popup_view-picture');
+const closeButton = document.querySelectorAll('.popup__close-button');
+const formElement = document.querySelector('.popup__content');
+const profileTitel = document.querySelector('.profile__title');
+const profileSubtitle = document.querySelector('.profile__subtitle');
+const inputFieldTitel = document.querySelector('.popup__input-field_type_titel');
+const inputFieldSubtitel = document.querySelector('.popup__input-field_type_subtitel');
+const likes = document.querySelectorAll('.element__like');
+const addPicture = document.querySelector('.profile__add-button');
 
-function togglePopup() {
-  popup.classList.toggle('popup_opened');
-  inputFieldTitel.value = profileTitel.textContent;
-  inputFieldSubtitel.value = profileSubtitle.textContent;
+function openPopup(popup) {
+  popup.classList.add('popup_opened');
 };
 
-editProfile.addEventListener('click', togglePopup);
-closeButton.addEventListener('click', togglePopup);
+editProfile.addEventListener('click', () => {
+  openPopup(popupProfile);
+});
+
+addPicture.addEventListener('click', () => {
+  openPopup(popupNewPic);
+});
+
+function closePopup(event) {
+  const theTarget = event.target;
+  const thePopup = theTarget.closest('.popup');
+  thePopup.classList.remove('popup_opened');
+};
+
+closeButton.forEach((button) => button.addEventListener('click', closePopup));
+
+// closeButton.forEach((button) => button.addEventListener('click', () => {
+//   closePopup(popupNewPic);
+// }));
+
+// closeButton.addEventListener('click', () => {
+//   closePopup(popupNewPic);
+// });
+
+
+
+// function openPopupNewPic() {
+//   popupNewPic.classList.add('popup_opened');
+// };
+
+// function openPopupViewPic() {
+//   popupViewPic.classList.add('popup_opened');
+// };
+
+// function closePopup() {
+//   popupProfile.classList.remove('popup_opened');
+//   popupNewPic.classList.remove('popup_opened');
+// };
+
+// editProfile.addEventListener('click', openPopupProfile);
+// closeButton.addEventListener('click', closePopup);
 
 function changeProfile(event) {
   event.preventDefault();
   profileTitel.textContent = inputFieldTitel.value;
   profileSubtitle.textContent = inputFieldSubtitel.value;
-  togglePopup();
+  closePopup(event);
 };
 
 formElement.addEventListener('submit', changeProfile);
+
+// addPicture.addEventListener('click', openPopupNewPic);
+
+//function changeProfile(event) {
+  //event.preventDefault();
+  //profileTitel.textContent = inputFieldTitel.value;
+  //profileSubtitle.textContent = inputFieldSubtitel.value;
+  //togglePopup();
+//};
+
+//formElement.addEventListener('submit', changeProfile);
 
 for (let i= 0; i < likes.length; i++) {
   likes[i].addEventListener('click', function() {
